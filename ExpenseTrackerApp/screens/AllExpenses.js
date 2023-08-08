821acb81fbbd8a32d21fe69902cd743d94f59413
Aleksandra Generowicz <o.generowicz@gmail.com>
2023-08-09 01:00:32 +0200

    Init project

---
+++ ExpenseTrackerApp/screens/AllExpenses.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function AllExpenses() {
+  return (
+    <View>
+      <Text>All expenses</Text>
+    </View>
+  );
+}
+
+export default AllExpenses;
---
+++ ExpenseTrackerApp/screens/ManageExpense.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function ManageExpense() {
+  return (
+    <View>
+      <Text>Manage expense</Text>
+    </View>
+  );
+}
+
+export default ManageExpense;
