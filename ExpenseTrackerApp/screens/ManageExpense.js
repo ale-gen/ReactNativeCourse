454db37f209528f4c7c0c96a45dde7acd50b8924
Aleksandra Generowicz <o.generowicz@gmail.com>
2023-08-10 17:10:27 +0200

    Close modal on buttons tap

--- ExpenseTrackerApp/screens/ManageExpense.js
+++ ExpenseTrackerApp/screens/ManageExpense.js
@@ -2,13 +2,21 @@ import { View, Text, StyleSheet } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Button from "../components/ui/Button";
 
-function ManageExpense({ route }) {
+function ManageExpense({ route, navigation }) {
   const insets = useSafeAreaInsets();
   const expenseId = route.params?.expenseId;
   const expenseName = route.params?.expenseName;
   const expenseAmount = route.params?.expenseAmount;
   const expenseDate = route.params?.expenseDate;
   const isEditing = !!expenseId;
+
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  function saveExpenseHandler() {
+    navigation.goBack();
+  }
 
   return (
     <View
@@ -19,8 +27,12 @@ function ManageExpense({ route }) {
     >
       <Text>Manage expense</Text>
       <View>
-        <Button mode={"flat"}>Cancel</Button>
-        <Button>{isEditing ? "Update" : "Create"}</Button>
+        <Button mode={"flat"} onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button onPress={saveExpenseHandler}>
+          {isEditing ? "Update" : "Create"}
+        </Button>
       </View>
     </View>
   );
